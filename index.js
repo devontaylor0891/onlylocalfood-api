b928aef0e26c9a90484a27baee13a0cae097cb1b
Nikki St Onge <nstonge@gasbuddy.com>
2017-03-04 10:29:39 -0600

    test

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use('/api', routes);
 app.get('/hikes', hike.index);
 app.post('/add_hike', hike.add_hike);
 
-app.listen(8000, function () {
-  console.log("Application is listening at http://localhost:8000/api");
+app.listen(8081, function () {
+  console.log("Application is listening at http://localhost:8081/api");
 })
