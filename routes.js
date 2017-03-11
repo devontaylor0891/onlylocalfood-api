54cd5a17fe20fc28b69ff02ccd3e8f6a484dbe03
Nikki St Onge <nstonge@gasbuddy.com>
2017-03-11 14:22:14 -0600

    all endpoints with an ID in them I don't know how to format so they don't work

--- routes.js
+++ routes.js
@@ -1,9 +1,39 @@
 var users = require('./src/handlers/users');
+var producers = require('./src/handlers/producers');
+var category = require('./src/handlers/category');
+var products = require('./src/handlers/products');
 var express = require('express');
 
 var router = express.Router();
 
-router.route('/users').get(users.getUsers); //just random routes to test
-router.route('/user').post(users.postUsers);
+router.route('/users').get(users.get_users);
+router.route('/users').post(users.post_users);
+router.route('/users/{id}').get(users.get_users_id);
+
+
+router.route('/producers/{id}').get(producers.get_producer_id);
+router.route('/producers/{id}').patch(producers.patch_producer_id);
+router.route('/producers/{id}/products').get(producers.get_producer_id_products);
+router.route('/producers/{id}/products').post(producers.post_producer_id_products);
+router.route('/producers/{id}/products/{id}').get(producers.get_producer_id_products_id);
+router.route('/producers/{id}/products/{id}/old').get(producers.get_producer_id_products_id_old);
+router.route('/producers/{id}/schedules').get(producers.get_producer_id_schedules);
+router.route('/producers/{id}/schedules').post(producers.post_producer_id_schedules);
+router.route('/producers/{id}/schedules/{id}').delete(producers.delete_producer_id_schedules_id);
+router.route('/producers/{id}/schedules/{id}').patch(producers.patch_producer_id_schedules_id);
+
+
+router.route('/category').get(category.get_category);
+
+
+router.route('/products').get(products.get_products);
+router.route('/products/{id}').patch(products.patch_products_id);
+
+
+router.route('/users/{id}/orders').get(users.get_users_id_orders);
+router.route('/users/{id}/orders/{id}').get(users.get_users_id_orders_id);
+// BODY: array of products (object with id and qty)
+router.route('/users/{id}/orders').post(users.post_users_id_orders);
+
 
 module.exports = router;
---
+++ src/handlers/category.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  get_category: function(req, res) {
+    return res.json({});
+  },
+};
