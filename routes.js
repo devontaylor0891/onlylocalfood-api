9f7d6d53f46a7e72ee9ed731f7604499f112f6e5
Nikki St Onge <nstonge@gasbuddy.com>
2017-09-11 22:15:04 -0600

    add /producers

--- routes.js
+++ routes.js
@@ -11,6 +11,7 @@ router.route('/users').get(users.get_users);
 router.route('/users').post(users.post_users);
 router.route('/users/:id').get(users.get_users_id);
 
+router.route('/producers').get(producers.get_producers);
 router.route('/producers/:id').get(producers.get_producers_id); // will have products in it and have schedules
 router.route('/producers/:id').patch(producers.patch_producer_id);
 router.route('/producers/:id/products').post(producers.post_producer_id_products);
--- src/handlers/producers.js
+++ src/handlers/producers.js
@@ -1,7 +1,31 @@
 'use strict';
+var connection = require('../../db');
 var Producer = require('./producers/{id}');
 
 module.exports = {
+  get_producers: function(req, res) {
+    connection.query(`
+    SELECT * FROM producers`, function (error, results) {
+      if (results.length === 0) {
+        res.status(404).send({ message: "Producers not found"});
+        return;
+      }
+      console.log("results 1:", results);
+      var products = results.map(function(row) {
+        return {
+          producerId: row.id,
+          producerName: row.name,
+          producerLogo: '/images/product.jpg',
+          producerLocation: row.city,
+          producerEmail: 'garden@farms.com',
+          producerPhone: row.phone_number,
+          producerDescription: 'This is the description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      });
+      res.status(200).send(products);
+    });
+  },
+
   get_producers_id: function(req, res) {
 		Producer.get_producers_id(req, res);
   },
