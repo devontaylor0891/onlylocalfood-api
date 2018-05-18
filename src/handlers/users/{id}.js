893d5f780011893231450751cc9b4d8b4592f3a9
Nikki St Onge <nstonge@gasbuddy.com>
2018-05-17 23:01:29 -0600

    Add one user endpoint

--- src/handlers/users/{id}.js
+++ src/handlers/users/{id}.js
@@ -4,22 +4,32 @@ var connection = require('../../../db');
 module.exports = {
   get_users_id: function(req, res) {
     var userId = req.params.id;
-    connection.query(
-      `SELECT * FROM users
-        WHERE id = ${userId}`,
-      function (error, results) {
-      if (results.length === 0) {
-        res.status(404).send({ message: "Product not found"});
+    connection.query(`
+      SELECT u.*, p.id AS producer_id
+      FROM users AS u
+      LEFT JOIN producers p
+        on p.user_id = u.id
+        WHERE u.id = ${userId}`,
+      function (error, userResult) {
+      if (userResult.length === 0) {
+        res.status(404).send({ message: "User not found"});
         return;
       }
-      console.log("results 1", results);
-      var user = {
-        id: results[0].id,
-        firstName: results[0].first_name,
-        email: results[0].email,
-        creationDate: results[0].registration_date,
-      };
-      res.status(200).send(user);
+      console.log("userResult 1", userResult);
+
+      connection.query(`SELECT * FROM orders WHERE user_id = ${userId}`, function (error, ordersResults) {
+        var user = userResult.map(function(row) {
+          return {
+            id: row.id,
+            firstName: row.first_name,
+            email: row.email,
+            registrationDate: row.registration_date,
+            role: row.producer_id ? 'producer' : 'consumer',
+            orders: ordersResults || [],
+          }
+        });
+        res.status(200).send(user);
+      });
     });
   },
 };
