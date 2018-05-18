846208617ca5c69714547b35116444743d86772b
Nikki St Onge <nstonge@gasbuddy.com>
2018-05-17 21:17:41 -0600

    f

--- db.js
+++ db.js
@@ -2,12 +2,21 @@ var mysql = require('mysql');
 
 var env = require('node-env-file');
 
+console.log("------------------");
+console.log("process.env.", process.env);
+console.log('----------------------');
 env(__dirname + '/.env');
+console.log("------------------");
+console.log("os.environ['RDS_HOSTNAME']", os.environ['RDS_HOSTNAME']);
+console.log("os.environ['RDS_USERNAME']", os.environ['RDS_USERNAME']);
+console.log("os.environ['RDS_PASSWORD']", os.environ['RDS_PASSWORD']);
+console.log("os.environ['RDS_PORT']", os.environ['RDS_PORT']);
+console.log('----------------------');
 var connection = mysql.createConnection({
   host: process.env.RDS_HOSTNAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME,  
+  database: process.env.RDS_DB_NAME, 
 });
 
 connection.connect(function(err) {
