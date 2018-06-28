5867213e261fbc60ef2acb2b85644233ad14896d
devontaylor0891 <devontaylor0891@gmail.com>
2018-06-27 19:15:34 -0600

    change users.js.

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -1,3 +1,64 @@
+// 'use strict';
+// var _ = require('lodash');
+// var connection = require('../../db');
+// var UserById = require('./users/{id}');
+// var UserByIdOrders = require('./users/{id}/orders');
+
+// module.exports = {
+//   get_users: function(req, res) {
+//     connection.query(`
+//       SELECT u.*, p.id AS producer_id
+//       FROM users AS u
+//       LEFT JOIN producers p
+//         on p.user_id = u.id`, function (error, usersResult) {
+//       if (usersResult.length === 0) {
+//         res.status(404).send({ message: "Users not found"});
+//         return;
+//       }
+//       console.log("usersResult 1:", usersResult);
+
+//       connection.query(`
+//         SELECT * FROM orders`, function (error, ordersResults) {
+//         const ordersGroupedBy = _(ordersResults)
+//           .groupBy('user_id')
+//           .value();
+
+//         var users = usersResult.map(function(row) {
+//           return {
+//             id: row.id,
+//             firstName: row.first_name,
+//             email: row.email,
+//             registrationDate: row.registration_date,
+//             role: row.producer_id ? 'producer' : 'consumer',
+//             orders: ordersGroupedBy[row.id] || [],
+//           }
+//         });
+//         res.status(200).send(users);
+//       });
+//     });
+//   },
+
+//   post_users: function(req, res) {
+//     return res.send(201);
+//   },
+
+//   get_users_id: function(req, res) {
+//     UserById.get_users_id(req, res);
+
+//   },
+
+//   get_users_id_orders: function(req, res) {
+//     UserByIdOrders.get_users_id_orders(req, res);
+//   },
+
+//   post_users_id_orders: function(req, res) {
+//     console.log(req.params.id);
+//     var userId = req.params.id;
+//     return res.send(201);
+//   },
+// };
+
+
 'use strict';
 var _ = require('lodash');
 var connection = require('../../db');
@@ -5,38 +66,62 @@ var UserById = require('./users/{id}');
 var UserByIdOrders = require('./users/{id}/orders');
 
 module.exports = {
-  get_users: function(req, res) {
-    connection.query(`
-      SELECT u.*, p.id AS producer_id
-      FROM users AS u
-      LEFT JOIN producers p
-        on p.user_id = u.id`, function (error, usersResult) {
+
+  get_users: function (req, res) {
+    connection.query(
+      `SELECT *
+      FROM ebdb.users`, function (error, usersResult) {
       if (usersResult.length === 0) {
         res.status(404).send({ message: "Users not found"});
         return;
       }
-      console.log("usersResult 1:", usersResult);
-
-      connection.query(`
-        SELECT * FROM orders`, function (error, ordersResults) {
-        const ordersGroupedBy = _(ordersResults)
-          .groupBy('user_id')
-          .value();
-
-        var users = usersResult.map(function(row) {
-          return {
-            id: row.id,
-            firstName: row.first_name,
-            email: row.email,
-            registrationDate: row.registration_date,
-            role: row.producer_id ? 'producer' : 'consumer',
-            orders: ordersGroupedBy[row.id] || [],
-          }
-        });
-        res.status(200).send(users);
+      console.log('usersResult: ', usersResult);
+      let users = usersResult.map(function(row) {
+        return {
+          id: row.id,
+          firstName: row.first_name,
+          email: row.email,
+          registrationDate: row.registration_date,
+          role: row.role,
+          orders: role.orders,
+          userId: role.user_id
+        }
       });
-    });
+      res.status(200).send(users);
+      });
   },
+  // get_users: function(req, res) {
+  //   connection.query(`
+  //     SELECT u.*, p.id AS producer_id 
+  //     FROM users AS u
+  //     LEFT JOIN producers p
+  //       on p.user_id = u.id`, function (error, usersResult) { // get the users from the db
+  //     if (usersResult.length === 0) { // error here
+  //       res.status(404).send({ message: "Users not found"});
+  //       return;
+  //     }
+  //     console.log("usersResult 1:", usersResult);
+
+  //     connection.query(`
+  //       SELECT * FROM orders`, function (error, ordersResults) {
+  //       const ordersGroupedBy = _(ordersResults)
+  //         .groupBy('user_id')
+  //         .value();
+
+  //       var users = usersResult.map(function(row) {
+  //         return {
+  //           id: row.id,
+  //           firstName: row.first_name,
+  //           email: row.email,
+  //           registrationDate: row.registration_date,
+  //           role: row.producer_id ? 'producer' : 'consumer',
+  //           orders: ordersGroupedBy[row.id] || [],
+  //         }
+  //       });
+  //       res.status(200).send(users);
+  //     });
+  //   });
+  // },
 
   post_users: function(req, res) {
     return res.send(201);
